4f8ef8172c1d19d2ffed3eb19cbcb0a24d067187
JuanSolisA <juan.solis.trabajo@gmail.com>
2022-05-25 23:30:45 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hola Mundo</h1>");
+})
+
+app.get('/amigos', (req, res) => {
+    res.send(amigos);
+})
+
+/* app.get('/index', (_req,res) => {
+    let htmlInfo = path.resolve(__dirname,'./vistas/index.html');
+    res.sendFile(htmlInfo);
+}) */
+app.listen(3001, () => console.log("Servidor escuchando en puerto 3001"));
